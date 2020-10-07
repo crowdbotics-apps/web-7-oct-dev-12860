daea327ea84090b543d1776f3a83c7a93ee73ab5
Crowdbotics <team@crowdbotics.com>
2020-10-07 10:47:54 +0000

    Update components

---
+++ src/features/BlankScreen111858/screens/blank.js
@@ -0,0 +1,59 @@
+import React from "react"
+import { View, Image, ImageBackground } from "react-native"
+import {
+  withStyles,
+  Text,
+  Button,
+  CheckBox,
+  Radio,
+  Toggle,
+  Icon,
+  Input,
+  Datepicker
+} from "react-native-ui-kitten"
+import Slider from "@react-native-community/slider"
+import { SlideMenuIcon } from "../../../navigator/slideMenuIcon"
+import { connect } from "react-redux"
+export class _Blank extends React.Component {
+  static navigationOptions = ({ navigation }) => {
+    return { headerLeft: <SlideMenuIcon navigationProps={navigation} /> }
+  }
+  state = { DateTimePicker_4: new Date("10/07/2020"), TextInput_5: "" }
+  render = () => (
+    <View style={styles.View_1}>
+      <Button
+        title="Press me!"
+        color="#3366FF"
+        style={styles.Button_2}
+        onPress={() => alert("Pressed!")}
+      />
+      <Text style={styles.Text_3}>Sample text content</Text>
+      <DateTimePicker
+        showIcon={false}
+        style={styles.DateTimePicker_4}
+        date={this.state.DateTimePicker_4}
+        onDateChange={selectedDate =>
+          this.setState({ DateTimePicker_4: selectedDate })
+        }
+      />
+      <TextInput
+        placeholder="Number Input Placeholder"
+        editable={true}
+        keyboardType="numeric"
+        style={styles.TextInput_5}
+        value={this.state.TextInput_5}
+        onChangeText={nextValue => this.setState({ TextInput_5: nextValue })}
+      />
+    </View>
+  )
+}
+
+Blank = withStyles(_Blank, theme => ({
+  container: { backgroundColor: theme["color-basic-100"] }
+}))
+
+function mapStateToProps(state) {
+  return { state: state.apiReducer }
+}
+const actionCreators = {}
+export default connect(mapStateToProps, actionCreators)(Blank)
